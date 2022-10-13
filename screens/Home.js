9bd9a789df3b51d46cc8df8b733e08a01573ab46
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-13 13:01:29 +1100

    add assets

--- screens/Home.js
+++ screens/Home.js
@@ -1,4 +1,5 @@
-import { Text } from "react-native";
+import { View, SafeArea, FlatList, Text } from "react-native";
+import { useState } from "react";
 
 const Home = () => {
   return <Text>Home</Text>;
