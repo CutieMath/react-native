2cb99473f80bd40200d7f0c2953de1d01cb5bf4f
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-13 14:05:23 +1100

    add circle button into the NFT card

---
+++ components/Button.js
@@ -0,0 +1,28 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { COLORS, SIZES, FONTS, SHADOWS } from "../constants";
+
+export const CircleButton = ({ imgUrl, handlePress, ...props }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        width: 40,
+        height: 40,
+        backgroundColor: COLORS.white,
+        position: "absolute",
+        borderRadius: SIZES.extraLarge,
+        alignItems: "center",
+        justifyContent: "center",
+        ...SHADOWS.light,
+        ...props,
+      }}
+    ></TouchableOpacity>
+  );
+};
+
+export const RectButton = () => {
+  return (
+    <View>
+      <Text>RectButton</Text>
+    </View>
+  );
+};
